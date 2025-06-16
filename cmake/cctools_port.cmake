72909787d03d8b208a4fcc2c1a8075b14f0db7d2
Alexander Cieslewicz <alexander.cieslewicz@gmail.com>
2025-06-16 12:56:43 -0600

    Switch to touchhle cctools-port
    This has the prebaked patches as well as windows build support

--- cmake/cctools_port.cmake
+++ cmake/cctools_port.cmake
@@ -1,67 +1,16 @@
 set(CCTOOLS_SOURCE_DIR "${SOURCES}/cctools-port")
 set(CCTOOLS_BUILD_DIR "${CMAKE_BINARY_DIR}/cctools-build")
 set(CCTOOLS_BUILD_PREFIX "${CMAKE_BINARY_DIR}/cctools")
-set(CCTOOLS_PATCH_FILE "${CMAKE_CURRENT_SOURCE_DIR}/patches/cctools.patch")
-set(CCTOOLS_PATCH_STAMP "${CMAKE_BINARY_DIR}/cctools_patch.stamp")
 
-file(MAKE_DIRECTORY ${CCTOOLS_BUILD_PREFIX})
-file(MAKE_DIRECTORY ${CCTOOLS_BUILD_DIR})
+include(ExternalProject)
 
-add_custom_command(
-    OUTPUT ${CCTOOLS_PATCH_STAMP}
-    COMMAND bash -c "if patch -p1 --dry-run -i \"${CCTOOLS_PATCH_FILE}\" > /dev/null 2>&1; then \
-                        patch -p1 -i \"${CCTOOLS_PATCH_FILE}\"; \
-                    else \
-                        echo \"Patch already applied or cannot be applied cleanly\"; \
-                    fi"
-    COMMAND ${CMAKE_COMMAND} -E touch "${CCTOOLS_PATCH_STAMP}"
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/sources/cctools-port
-    COMMENT "Patching cctools-port"
-    DEPENDS ${CCTOOLS_PATCH_FILE}
-    VERBATIM
-)
-
-add_custom_command(
-    OUTPUT ${CCTOOLS_SOURCE_DIR}/cctools/configure
-    COMMAND ./autogen.sh
-    WORKING_DIRECTORY ${CCTOOLS_SOURCE_DIR}/cctools
-    COMMENT "Running cctools autogen.sh"
-    DEPENDS ${CCTOOLS_PATCH_STAMP}
-)
-
-add_custom_command(
-    OUTPUT ${CCTOOLS_BUILD_DIR}/config.status
-    COMMAND ${CMAKE_COMMAND} -E make_directory ${CCTOOLS_BUILD_DIR}
-    COMMAND ${CCTOOLS_SOURCE_DIR}/cctools/configure --prefix=${CCTOOLS_BUILD_PREFIX} --target=arm-apple-darwin10
-    WORKING_DIRECTORY ${CCTOOLS_BUILD_DIR}
-    COMMENT "Configuring cctools build environment"
-    DEPENDS ${CCTOOLS_SOURCE_DIR}/cctools/configure
-)
-
-add_custom_target(cctools_port
-    COMMAND make -j${CMAKE_BUILD_PARALLEL_LEVEL}
-    COMMAND make install
-    WORKING_DIRECTORY ${CCTOOLS_BUILD_DIR}
-    COMMENT "Building cctools-port"
-    DEPENDS ${CCTOOLS_BUILD_DIR}/config.status
-)
-
-add_custom_command(
-    TARGET cctools_port
-    POST_BUILD
-    COMMAND bash -c "for file in arm-apple-darwin10-*; do \
-                        target_name=\${file#arm-apple-darwin10-}; \
-                        mv \$file \$target_name; \
-                        echo \"Renamed binary: \$file -> \$target_name\"; \
-                    done"
-    WORKING_DIRECTORY ${CCTOOLS_BUILD_PREFIX}/bin
-    COMMENT "Removing prefix from tool binary names"
-    VERBATIM
-)
-
-install(
-    DIRECTORY ${CCTOOLS_BUILD_PREFIX}/
-    DESTINATION ${SDK_PATH}/usr
-    USE_SOURCE_PERMISSIONS
-    COMPONENT cctools
+ExternalProject_Add(cctools_port
+    SOURCE_DIR ${CCTOOLS_SOURCE_DIR}
+    BINARY_DIR ${CCTOOLS_BUILD_DIR}
+    INSTALL_DIR ${CCTOOLS_BUILD_PREFIX}
+    CMAKE_ARGS
+        -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+        -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+        -DCMAKE_INSTALL_PREFIX=${CCTOOLS_BUILD_PREFIX}
+        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
 )
