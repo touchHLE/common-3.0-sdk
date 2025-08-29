21a12d4c6319dbbc1940b2afcdfeb3836014f6c8
Alexander Cieslewicz <alexander.cieslewicz@gmail.com>
2025-08-29 19:05:27 +0300

    feat(libc_stub): allow stub.c generator to take multiple inputs

--- sources/libc-stub/generate_stub.py
+++ sources/libc-stub/generate_stub.py
@@ -4,35 +4,44 @@ import argparse
 
 from pathlib import Path
 
-from typing import List
+from typing import List, Set
 
-def generate_stub(input_file: Path | str, output_file: Path | str) -> None:
-    input_file, output_file = Path(input_file), Path(output_file)
 
-    with open(input_file, "r") as f:
-        symbols: List[str] = f.readlines()
+def generate_stub(input_files: List[Path | str], output_file: Path | str) -> None:
+    input_files, output_file = [Path(file) for file in input_files], Path(output_file)
 
-    symbols = [f"void {symbol[1:].strip()}() {{}}\n" for symbol in symbols]
+    symbols: Set[str] = set()
+    for input_file in input_files:
+        with open(input_file, "r") as f:
+            file_symbols: List[str] = f.readlines()
+        for symbol in file_symbols:
+            if symbol in symbols:
+                print(f"WARN: Duplicate symbol ({symbol.strip()}) found in {input_file}.")
+            symbols.add(symbol)
+
+    stub_functions: List[str] = [
+        f"void {symbol[1:].strip()}() {{}}\n" for symbol in symbols
+    ]
+    stub_functions.sort()
 
     with open(output_file, "w", encoding="utf-8") as f:
-        f.writelines(symbols)
+        f.writelines(stub_functions)
+
 
 def main() -> None:
     parser = argparse.ArgumentParser(
         description="Convert a list of symbols to a stub c file"
     )
     parser.add_argument(
-        "symbol_list",
+        "symbol_lists",
         help="A list of symbols to use to generate a stub c file",
-        type=Path
+        type=Path,
+        nargs="+",
     )
-    parser.add_argument(
-        "output",
-        help="Output file name.",
-        type=Path
-    )
+    parser.add_argument("output", help="Output file name.", type=Path)
     args = parser.parse_args()
-    generate_stub(args.symbol_list, args.output)
+    generate_stub(args.symbol_lists, args.output)
+
 
 if __name__ == "__main__":
     main()
