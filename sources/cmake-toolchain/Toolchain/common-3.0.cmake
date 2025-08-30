b72aa5e5f8c957fc3d32c93ab1b430c88386e41f
Alexander Cieslewicz <55669270+acieslewicz@users.noreply.github.com>
2025-08-30 05:43:58 -0600

    Setup integration test using zlib dylib build (#14)
    * Upload tar.gz to preserver file perms
    
    * Change compiler requirements to be more accurate
    
    * Replace -isysroot flag with setting CMAKE_SYSROOT
    
    * Setup sdk test via building zlib
    
    * Allow env to overwrite compiler in toolchain
    
    * Set git to use LF

--- sources/cmake-toolchain/Toolchain/common-3.0.cmake
+++ sources/cmake-toolchain/Toolchain/common-3.0.cmake
@@ -5,7 +5,19 @@ set(CMAKE_SYSTEM_NAME common-3.0)
 set(CMAKE_SYSTEM_VERSION 3.0)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(CMAKE_C_COMPILER clang)
+set(CMAKE_SYSROOT ${SDK_PATH})
+
+if(DEFINED ENV{CC})
+    set(CMAKE_C_COMPILER $ENV{CC})
+else()
+    set(CMAKE_C_COMPILER clang)
+endif()
+
+if(DEFINED ENV{CXX})
+    set(CMAKE_CXX_COMPILER $ENV{CXX})
+else()
+    set(CMAKE_CXX_COMPILER clang++)
+endif()
 
 set(CMAKE_C_COMPILER_TARGET arm-apple-ios3.0)
 
@@ -16,7 +28,7 @@ endif()
 string(REPLACE ";" " -arch " ARCH_FLAGS "-arch ${CMAKE_ARCHITECTURES}")
 
 set(CMAKE_C_FLAGS_INIT
-    "-isysroot ${SDK_PATH} -B${SDK_PATH}/usr/bin ${ARCH_FLAGS} -Wno-incompatible-sysroot -mlinker-version=253 -mfpu=vfpv2"
+    "-B${SDK_PATH}/usr/bin ${ARCH_FLAGS} -Wno-incompatible-sysroot -mlinker-version=253 -mfpu=vfpv2"
 )
 
 set(CMAKE_FIND_ROOT_PATH ${SDK_PATH})
